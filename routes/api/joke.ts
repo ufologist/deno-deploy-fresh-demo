3486a07c660d4c0f432e3e3e10df490aa5187f43
Sun <klsforever@gmail.com>
2023-06-09 10:41:15 +0000

    fix: joke api

--- routes/api/joke.ts
+++ routes/api/joke.ts
@@ -1,18 +1,49 @@
 import { HandlerContext } from "$fresh/server.ts";
+import Authenticator from 'npm:claude-api@1.0.4';
 
-// Jokes courtesy of https://punsandoneliners.com/randomness/programmer-jokes/
-const JOKES = [
-  "Why do Java developers often wear glasses? They can't C#.",
-  "A SQL query walks into a bar, goes up to two tables and says “can I join you?”",
-  "Wasn't hard to crack Forrest Gump's password. 1forrest1.",
-  "I love pressing the F5 key. It's refreshing.",
-  "Called IT support and a chap from Australia came to fix my network connection.  I asked “Do you come from a LAN down under?”",
-  "There are 10 types of people in the world. Those who understand binary and those who don't.",
-  "Why are assembly programmers often wet? They work below C level.",
-  "My favourite computer based band is the Black IPs.",
-  "What programme do you use to predict the music tastes of former US presidential candidates? An Al Gore Rhythm.",
-  "An SEO expert walked into a bar, pub, inn, tavern, hostelry, public house.",
-];
+async function askCluadeAPi(question, conversationId) {
+  const token = Deno.env.get("token");
+  const bot = Deno.env.get("bot");
+  // 频道的名字(可以是不存在的频道, 如果是已存在的频道必须添加了 Claude)
+  const chatId = Deno.env.get("chatId");
+
+  // 初始化claude
+  const claudeClient = new Authenticator(token, bot);
+
+  // 创建频道并返回房间ID：channel
+  const channel = await claudeClient.newChannel(chatId);
+
+  let result;
+  if (conversationId) {
+    result = await claudeClient.sendMessage({
+      text: question,
+      channel,
+      conversationId,
+      onMessage: (originalMessage) => {
+        console.log('loading', originalMessage);
+      },
+    });
+  } else {
+    result = await claudeClient.sendMessage({
+      text: question,
+      channel,
+      onMessage: (originalMessage) => {
+        // console.log("loading", originalMessage)
+        console.log('loading', originalMessage);
+      },
+    });
+  }
+  console.log('success', result);
+
+  return {
+    status: 0,
+    data: {
+      text: result.text,
+      conversationId: result.conversationId,
+    },
+    message: 'success',
+  };
+}
 
 export const handler = async (_req: Request, _ctx: HandlerContext): Response => {
   const url = new URL(_req.url);
@@ -21,11 +52,16 @@ export const handler = async (_req: Request, _ctx: HandlerContext): Response => 
   const question = url.searchParams.get('question');
   const conversationId = url.searchParams.get('conversationId');
 
-  const randomIndex = Math.floor(Math.random() * JOKES.length);
-  const body = JOKES[randomIndex];
+  let result = {};
+  try {
+    result = await askCluadeAPi(question, conversationId);
+  } catch (error) {
+    result = {
+      status: 1,
+      data: null,
+      message: `调用 AI 接口失败: ${error.message}`,
+    };
+  }
 
-  const response = (await fetch(`https://jxzptkyr84.hk.aircode.run/completion?question=${encodeURIComponent(String(question))}`));
-  const json = await response.json();
-
-  return new Response(JSON.stringify(json));
+  return new Response(JSON.stringify(result));
 };
